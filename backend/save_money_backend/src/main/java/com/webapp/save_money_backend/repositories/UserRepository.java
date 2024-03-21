4750e5119470f2a2ebb3216701b14a2f30c58f53
vega178 <Esteban.Vega@paramount.com>
2024-03-21 16:59:18 -0500

    Login with JPA

--- backend/save_money_backend/src/main/java/com/webapp/save_money_backend/repositories/UserRepository.java
+++ backend/save_money_backend/src/main/java/com/webapp/save_money_backend/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.webapp.save_money_backend.repositories;
 
 import com.webapp.save_money_backend.models.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
@@ -8,4 +9,8 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User, Long> {
     @Override
     Optional<User> findById(Long id);
+
+    //Consulta a la clase entities (?1: se refiere a consultar el valor del parametro username)
+    //@Query("select u from User u where u.username=?1") //esto cuando no se escribe el metodo con el verbo por getUserByUserName
+    Optional<User> findByUsername(String username);
 }
--- backend/save_money_backend/src/main/java/com/webapp/save_money_backend/services/JpaUserDetailsService.java
+++ backend/save_money_backend/src/main/java/com/webapp/save_money_backend/services/JpaUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.webapp.save_money_backend.services;
 
+import com.webapp.save_money_backend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -7,22 +9,31 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (!username.equals("admin")) {
+        //Implementación del login con JPA
+        Optional<com.webapp.save_money_backend.models.entities.User> userEntities = repository.findByUsername(username);
+        if (!userEntities.isPresent()) {
             throw new UsernameNotFoundException(String.format("User %s not found", username));
         }
-
+        com.webapp.save_money_backend.models.entities.User user = userEntities.orElseThrow();
         List<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-        return new User(username,
-                "$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS",
+        return new User(user.getUsername(),
+                user.getPassword(),
                 true,
                 true,
                 true,
